d98cb4be060617067a6bffb892233fb98b70ad39
seokho <seokho7799@gmail.com>
2025-04-04 05:50:34 +0900

    docs: 상위 상품 조회 API 문서화 추가

--- src/main/java/kr/hhplus/be/server/controller/spec/ProductControllerSpec.java
+++ src/main/java/kr/hhplus/be/server/controller/spec/ProductControllerSpec.java
@@ -35,4 +35,28 @@ public interface ProductControllerSpec {
       )
   })
   CommonResponseWrapper<List<ProductSummaryResponse>> findAllProducts();
+
+  @Operation(
+      summary = "상위 판매 상품 조회",
+      description = "상위 판매 상품을 조회합니다.",
+      tags = {"상품"}
+  )
+  @ApiResponses(value = {
+      @ApiResponse(
+          responseCode = "200",
+          description = "상위 판매 상품 조회 성공",
+          useReturnTypeSchema = true
+      ),
+      @ApiResponse(
+          responseCode = "500",
+          description = "서버 오류",
+          content = @Content(
+              mediaType = "application/json",
+              schema = @Schema(
+                  implementation = ErrorResponse.class
+              )
+          )
+      )
+  })
+  CommonResponseWrapper<ProductSummaryResponse> findTopSellingProducts();
 }
