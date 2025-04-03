b6b82d08ae3696d27b4b42de18074f9a8648df19
seokho <seokho7799@gmail.com>
2025-04-04 01:58:17 +0900

    chore: 스웨거 설정

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,10 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 
-    // DB
+	// Swagger
+	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.6")
+
+	// DB
 	runtimeOnly("com.mysql:mysql-connector-j")
 
     // Test
