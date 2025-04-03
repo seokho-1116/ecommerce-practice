240b53852e239efb3b13a778bd35feb43e50e5a2
seokho <seokho7799@gmail.com>
2025-04-04 05:59:25 +0900

    fix: @RequestBody 누락 추가

--- src/main/java/kr/hhplus/be/server/controller/OrderController.java
+++ src/main/java/kr/hhplus/be/server/controller/OrderController.java
@@ -23,7 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class OrderController implements OrderControllerSpec {
 
   @PostMapping
-  public CommonResponseWrapper<OrderResponse> createOrder(OrderRequest request) {
+  public CommonResponseWrapper<OrderResponse> createOrder(@RequestBody OrderRequest request) {
     return CommonResponseWrapper.ok(
         new OrderResponse(
             1L,
