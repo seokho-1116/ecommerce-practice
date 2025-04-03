e165f4d23fa6bfa8fe26a1fae707d89ea8c2c71e
seokho <seokho7799@gmail.com>
2025-04-04 03:35:52 +0900

    feat: 쿠폰 API Mock

---
+++ src/main/java/kr/hhplus/be/server/controller/CouponController.java
@@ -0,0 +1,54 @@
+package kr.hhplus.be.server.controller;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import kr.hhplus.be.server.controller.response.CommonResponseWrapper;
+import kr.hhplus.be.server.controller.response.CouponIssueResponse;
+import kr.hhplus.be.server.controller.response.CouponSummaryResponse;
+import kr.hhplus.be.server.service.CouponType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/coupons")
+public class CouponController {
+
+  @GetMapping
+  public CommonResponseWrapper<List<CouponSummaryResponse>> findAllCoupons() {
+    return CommonResponseWrapper.ok(
+        List.of(
+            new CouponSummaryResponse(
+                1L,
+                1L,
+                "쿠폰 이름",
+                0.1,
+                null,
+                CouponType.PERCENTAGE,
+                LocalDateTime.now(),
+                LocalDateTime.now().plusDays(1),
+                LocalDateTime.now()
+            )
+        )
+    );
+  }
+
+  @PostMapping("/issue")
+  public CommonResponseWrapper<CouponIssueResponse> issue() {
+    return CommonResponseWrapper.ok(
+        new CouponIssueResponse(
+            1L,
+            1L,
+            1L,
+            "쿠폰 이름",
+            null,
+            1000L,
+            CouponType.FIXED,
+            LocalDateTime.now(),
+            LocalDateTime.now().plusDays(1),
+            LocalDateTime.now()
+        )
+    );
+  }
+}
---
+++ src/main/java/kr/hhplus/be/server/controller/response/CouponSummaryResponse.java
@@ -0,0 +1,18 @@
+package kr.hhplus.be.server.controller.response;
+
+import java.time.LocalDateTime;
+import kr.hhplus.be.server.service.CouponType;
+
+public record CouponSummaryResponse(
+    Long userId,
+    Long userCouponId,
+    String couponName,
+    Double discountRate,
+    Long discountAmount,
+    CouponType couponType,
+    LocalDateTime from,
+    LocalDateTime to,
+    LocalDateTime createdAt
+) {
+
+}
