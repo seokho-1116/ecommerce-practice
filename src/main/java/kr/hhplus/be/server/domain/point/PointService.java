5a2e3f20fdc2c94a2a9e3b02bbc9689ec4913d6f
seokho <seokho7799@gmail.com>
2025-04-11 00:54:49 +0900

    fix: 충전/사용 시 잔액 반환하도록 수정

--- src/main/java/kr/hhplus/be/server/domain/point/PointService.java
+++ src/main/java/kr/hhplus/be/server/domain/point/PointService.java
@@ -8,7 +8,7 @@ public class PointService {
 
   private PointRepository pointRepository;
 
-  public void use(Long userId, Long amount) {
+  public long use(Long userId, Long amount) {
     if (userId == null) {
       throw new PointBusinessException("유저 ID는 null일 수 없습니다.");
     }
@@ -22,5 +22,34 @@ public class PointService {
     pointRepository.savePointHistory(pointHistory);
 
     pointRepository.save(userPoint);
+
+    return userPoint.getAmount();
+  }
+
+  public long charge(Long userId, Long amount) {
+    if (userId == null) {
+      throw new PointBusinessException("유저 ID는 null일 수 없습니다.");
+    }
+
+    UserPoint userPoint = pointRepository.findByUserId(userId)
+        .orElseThrow(() -> new UserPointNotFoundException("유저 포인트를 찾을 수 없습니다."));
+
+    userPoint.charge(amount);
+
+    PointHistory pointHistory = PointHistory.chargeHistory(userPoint.getUser(), amount);
+    pointRepository.savePointHistory(pointHistory);
+
+    pointRepository.save(userPoint);
+
+    return userPoint.getAmount();
+  }
+
+  public UserPoint findUserPointByUserId(Long userId) {
+    if (userId == null) {
+      throw new PointBusinessException("유저 ID는 null일 수 없습니다.");
+    }
+
+    return pointRepository.findByUserId(userId)
+        .orElseThrow(() -> new UserPointNotFoundException("유저 포인트를 찾을 수 없습니다."));
   }
 }
