ce3eede434235e2bae18340853af2faca4695a26
seokho <seokho7799@gmail.com>
2025-04-11 01:42:02 +0900

    docs: 주문 API 응답 문서화 추가

--- src/main/java/kr/hhplus/be/server/interfaces/order/OrderResponse.java
+++ src/main/java/kr/hhplus/be/server/interfaces/order/OrderResponse.java
@@ -1,5 +1,6 @@
 package kr.hhplus.be.server.interfaces.order;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -14,13 +15,28 @@ public record OrderResponse(
 ) {
 
   public record OrderSuccessResponse(
+      @Schema(description = "주문 ID")
       Long orderId,
+
+      @Schema(description = "유저 ID")
       Long userId,
+
+      @Schema(description = "주문 상태")
       OrderStatus status,
+
+      @Schema(description = "주문 총 가격")
       Long totalPrice,
+
+      @Schema(description = "주문 할인 가격")
       Long discountPrice,
+
+      @Schema(description = "주문 최종 가격")
       Long finalPrice,
+
+      @Schema(description = "쿠폰 정보")
       CouponInfoResponse couponInfo,
+
+      @Schema(description = "주문 상품 목록")
       List<ItemInfoResponse> itemInfos
   ) {
 
@@ -72,24 +88,50 @@ public record OrderResponse(
     }
 
     public record ItemInfoResponse(
+        @Schema(description = "주문 상품 ID")
         Long orderItemId,
+
+        @Schema(description = "상품 이름")
         String productName,
+
+        @Schema(description = "상품 설명")
         String productDescription,
+
+        @Schema(description = "상품 옵션 이름")
         String productOptionName,
+
+        @Schema(description = "상품 옵션 설명")
         String productOptionDescription,
+
+        @Schema(description = "상품 기본 가격")
         Long basePrice,
+
+        @Schema(description = "상품 옵션 추가 가격")
         Long additionalPrice,
+
+        @Schema(description = "상품 총 가격")
         Long totalPrice,
+
+        @Schema(description = "상품 수량")
         Long quantity
     ) {
 
     }
 
     public record CouponInfoResponse(
+        @Schema(description = "쿠폰 ID")
         Long couponId,
+
+        @Schema(description = "쿠폰 이름")
         String couponName,
+
+        @Schema(description = "쿠폰 할인율")
         Double discountRate,
+
+        @Schema(description = "쿠폰 할인 금액")
         Long discountAmount,
+
+        @Schema(description = "쿠폰 타입")
         CouponType couponType
     ) {
 
