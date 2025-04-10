067bb2da227ffb8a2d2f9182cad90afc92ddd6cf
seokho <seokho7799@gmail.com>
2025-04-10 23:20:48 +0900

    test: 포인트 도메인 서비스 테스트 추가

--- src/test/java/kr/hhplus/be/server/domain/point/PointServiceTest.java
+++ src/test/java/kr/hhplus/be/server/domain/point/PointServiceTest.java
@@ -54,4 +54,36 @@ class PointServiceTest {
     assertThatThrownBy(() -> pointService.use(userId, amount))
         .isInstanceOf(PointBusinessException.class);
   }
+
+  @DisplayName("포인트 충전 시 포인트 내역이 저장되어야 한다")
+  @Test
+  void chargePointTest() {
+    // given
+    Long userId = 1L;
+    Long amount = 100L;
+    UserPoint userPoint = UserPoint.builder()
+        .amount(1000L)
+        .build();
+
+    when(pointRepository.findByUserId(userId))
+        .thenReturn(Optional.of(userPoint));
+
+    // when
+    pointService.charge(userId, amount);
+
+    // then
+    verify(pointRepository, atLeastOnce()).savePointHistory(any(PointHistory.class));
+  }
+
+  @DisplayName("포인트 충전 시 유저 아이디가 null인 경우 예외가 발생해야 한다")
+  @Test
+  void chargePoint_NullUserId_ThrowsException() {
+    // given
+    Long userId = null;
+    Long amount = 100L;
+
+    // when & then
+    assertThatThrownBy(() -> pointService.charge(userId, amount))
+        .isInstanceOf(PointBusinessException.class);
+  }
 }
