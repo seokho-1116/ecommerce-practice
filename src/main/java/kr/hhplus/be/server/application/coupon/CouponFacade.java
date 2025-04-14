5c25f531e920ed12be1dd32a85c1f4f584b205fd
seokho <seokho7799@gmail.com>
2025-04-14 21:37:00 +0900

    fix: 의존성 final로 설정

--- src/main/java/kr/hhplus/be/server/application/coupon/CouponFacade.java
+++ src/main/java/kr/hhplus/be/server/application/coupon/CouponFacade.java
@@ -4,13 +4,15 @@ import kr.hhplus.be.server.domain.coupon.CouponService;
 import kr.hhplus.be.server.domain.coupon.UserCoupon;
 import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.domain.user.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class CouponFacade {
 
-  private UserService userService;
-  private CouponService couponService;
+  private final UserService userService;
+  private final CouponService couponService;
 
   public UserCoupon issue(Long userId, Long couponId) {
     User user = userService.findUserById(userId);
