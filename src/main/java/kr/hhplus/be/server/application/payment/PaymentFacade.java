ddbe9c8bc8b25dac8890a63fa776dafcc8c663a9
seokho <seokho7799@gmail.com>
2025-04-18 05:59:00 +0900

    fix: 트랜잭션에서 결제 플랫폼 전송에서 분리

--- src/main/java/kr/hhplus/be/server/application/payment/PaymentFacade.java
+++ src/main/java/kr/hhplus/be/server/application/payment/PaymentFacade.java
@@ -1,6 +1,5 @@
 package kr.hhplus.be.server.application.payment;
 
-import jakarta.transaction.Transactional;
 import kr.hhplus.be.server.domain.order.Order;
 import kr.hhplus.be.server.domain.order.OrderService;
 import kr.hhplus.be.server.domain.payment.PaymentCommand;
@@ -11,6 +10,7 @@ import kr.hhplus.be.server.domain.product.ProductDeductCommand;
 import kr.hhplus.be.server.domain.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionTemplate;
 
 @Component
 @RequiredArgsConstructor
@@ -20,21 +20,24 @@ public class PaymentFacade {
   private final ProductService productService;
   private final PointService pointService;
   private final PaymentEventPublisher paymentEventPublisher;
+  private final TransactionTemplate transactionTemplate;
 
-  @Transactional
   public PaymentResult payOrder(PaymentCommand paymentCommand) {
-    Order order = orderService.findNotPaidOrderById(paymentCommand.orderId());
+    PaymentResult result = transactionTemplate.execute(status -> {
+      Order order = orderService.findNotPaidOrderById(paymentCommand.orderId());
 
-    ProductDeductCommand productDeductCommand = ProductDeductCommand.from(order.getOrderItems());
-    productService.deductInventory(productDeductCommand);
+      ProductDeductCommand productDeductCommand = ProductDeductCommand.from(order.getOrderItems());
+      productService.deductInventory(productDeductCommand);
 
-    long remainingPoint = pointService.use(paymentCommand.userId(), order.getFinalPrice());
+      long remainingPoint = pointService.use(paymentCommand.userId(), order.getFinalPrice());
 
-    orderService.pay(order);
+      orderService.pay(order);
+      return PaymentResult.of(order, remainingPoint);
+    });
 
-    PaymentSuccessEvent event = PaymentSuccessEvent.from(order);
+    PaymentSuccessEvent event = PaymentSuccessEvent.from(result.order());
     paymentEventPublisher.publish(event);
 
-    return PaymentResult.of(order, remainingPoint);
+    return result;
   }
 }
