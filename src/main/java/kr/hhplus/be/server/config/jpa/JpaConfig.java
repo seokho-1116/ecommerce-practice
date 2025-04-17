b68f9f03adae3071bd69f8ac5683b9b2e75b7b9e
seokho <seokho7799@gmail.com>
2025-04-18 04:44:26 +0900

    Revert "fix: jpa config 경로 변경"
    This reverts commit 6da5c04ee2cefb50733ea69c71432260f59b59b1.

--- src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
+++ src/main/java/kr/hhplus/be/server/config/jpa/JpaConfig.java
@@ -9,7 +9,7 @@ import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @EnableJpaAuditing
-@EnableJpaRepositories(basePackages = "kr.hhplus.be.server.infrastructure")
+@EnableJpaRepositories
 public class JpaConfig {
 
     @Bean
