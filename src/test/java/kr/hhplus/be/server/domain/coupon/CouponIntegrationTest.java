ae06a32cbe23a6ff4b90c2ab29d6a855283f87ac
seokho <seokho7799@gmail.com>
2025-04-21 23:59:31 +0900

    test: Coupon 도메인 테스트 리팩터링
    - 기존 @SQL에서 애플리케이션 수준에서 삽입하도록 변경

---
+++ src/test/java/kr/hhplus/be/server/domain/coupon/CouponIntegrationTest.java
@@ -0,0 +1,124 @@
+package kr.hhplus.be.server.domain.coupon;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.atomic.AtomicInteger;
+import kr.hhplus.be.server.IntegrationTestSupport;
+import kr.hhplus.be.server.common.TestReflectionUtil;
+import kr.hhplus.be.server.domain.user.User;
+import kr.hhplus.be.server.domain.user.UserTestDataGenerator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+class CouponIntegrationTest extends IntegrationTestSupport {
+
+  @Autowired
+  private CouponService couponService;
+
+  @Autowired
+  private CouponTestDataGenerator couponTestDataGenerator;
+
+  @Autowired
+  private UserTestDataGenerator userTestDataGenerator;
+
+  private UserCoupon userCoupon;
+  private User user;
+  private Coupon coupon;
+
+  @BeforeEach
+  void setup() {
+    user = userTestDataGenerator.user();
+    testHelpRepository.save(user);
+
+    coupon = couponTestDataGenerator.validateCoupon();
+    long randomQuantity = ThreadLocalRandom.current().nextLong(1, 6);
+    TestReflectionUtil.setField(coupon, "quantity", randomQuantity);
+    testHelpRepository.save(coupon);
+
+    userCoupon = couponTestDataGenerator.notUsedUserCoupon(user, coupon);
+    testHelpRepository.save(userCoupon);
+  }
+
+  @DisplayName("쿠폰을 사용하면 쿠폰이 사용된다")
+  @Test
+  void useTest() {
+    // given
+    // when
+    couponService.use(userCoupon);
+
+    // then
+    assertThat(userCoupon.getIsUsed()).isTrue();
+  }
+
+  @DisplayName("쿠폰을 발급하면 쿠폰이 발급된다")
+  @Test
+  void issueTest() {
+    // given
+    // when
+    UserCoupon issuedCoupon = couponService.issue(user, coupon.getId());
+
+    // then
+    assertThat(issuedCoupon.getIsUsed()).isFalse();
+  }
+
+  @DisplayName("동시에 쿠폰을 사용하면 쿠폰이 한 장만 사용된다")
+  @Test
+  void concurrentUseTest() throws InterruptedException {
+    // given
+    int concurrentRequest = 10;
+    CountDownLatch latch = new CountDownLatch(concurrentRequest);
+
+    // when
+    AtomicInteger successCount = new AtomicInteger(0);
+    AtomicInteger failureCount = new AtomicInteger(0);
+    for (int i = 0; i < concurrentRequest; i++) {
+      new Thread(() -> {
+        try {
+          couponService.use(userCoupon);
+
+          successCount.incrementAndGet();
+        } catch (Exception e) {
+          failureCount.incrementAndGet();
+        } finally {
+          latch.countDown();
+        }
+      }).start();
+    }
+    latch.await();
+
+    // then
+    assertThat(successCount.get()).isEqualTo(1);
+  }
+
+  @DisplayName("동시에 쿠폰을 발급하면 쿠폰이 수량만큼만 발급된다")
+  @Test
+  void concurrentIssueTest() throws InterruptedException {
+    // given
+    int concurrentRequest = 10;
+    CountDownLatch latch = new CountDownLatch(concurrentRequest);
+
+    // when
+    AtomicInteger successCount = new AtomicInteger(0);
+    AtomicInteger failureCount = new AtomicInteger(0);
+    for (int i = 0; i < concurrentRequest; i++) {
+      new Thread(() -> {
+        try {
+          couponService.issue(user, coupon.getId());
+          successCount.incrementAndGet();
+        } catch (Exception e) {
+          failureCount.incrementAndGet();
+        } finally {
+          latch.countDown();
+        }
+      }).start();
+    }
+    latch.await();
+
+    // then
+    assertThat(successCount.longValue()).isEqualTo(coupon.getQuantity());
+  }
+}
--- src/test/java/kr/hhplus/be/server/domain/coupon/CouponTestDataGenerator.java
+++ src/test/java/kr/hhplus/be/server/domain/coupon/CouponTestDataGenerator.java
@@ -2,6 +2,7 @@ package kr.hhplus.be.server.domain.coupon;
 
 import static org.instancio.Select.field;
 
+import java.time.LocalDateTime;
 import kr.hhplus.be.server.domain.user.User;
 import org.instancio.Instancio;
 import org.springframework.stereotype.Component;
@@ -18,10 +19,34 @@ public class CouponTestDataGenerator {
         .create();
   }
 
+  public Coupon validateCoupon() {
+    LocalDateTime now = LocalDateTime.now();
+    return Instancio.of(Coupon.class)
+        .ignore(field(Coupon::getId))
+        .set(field(Coupon::getIsActive), true)
+        .set(field(Coupon::getFrom), now.minusDays(3))
+        .set(field(Coupon::getTo), now.plusDays(3))
+        .ignore(field(Coupon::getCreatedAt))
+        .ignore(field(Coupon::getUpdatedAt))
+        .create();
+  }
+
   public UserCoupon userCoupon(User user, Coupon coupon) {
     return Instancio.of(UserCoupon.class)
         .ignore(field(UserCoupon::getId))
         .set(field(UserCoupon::getIsActive), true)
+        .ignore(field(UserCoupon::getCreatedAt))
+        .ignore(field(UserCoupon::getUpdatedAt))
+        .set(field(UserCoupon::getUser), user)
+        .set(field(UserCoupon::getCoupon), coupon)
+        .create();
+  }
+
+  public UserCoupon notUsedUserCoupon(User user, Coupon coupon) {
+    return Instancio.of(UserCoupon.class)
+        .ignore(field(UserCoupon::getId))
+        .set(field(UserCoupon::getIsActive), true)
+        .set(field(UserCoupon::getIsUsed), false)
         .ignore(field(UserCoupon::getCreatedAt))
         .ignore(field(UserCoupon::getUpdatedAt))
         .set(field(UserCoupon::getUser), user)
