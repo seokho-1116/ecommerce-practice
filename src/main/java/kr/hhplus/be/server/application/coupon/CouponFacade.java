7f9811e125798342023390297022c0f794821f26
seokho <seokho7799@gmail.com>
2025-05-03 11:20:53 +0900

    feat: 쿠폰 발급 분산락 추가

--- src/main/java/kr/hhplus/be/server/application/coupon/CouponFacade.java
+++ src/main/java/kr/hhplus/be/server/application/coupon/CouponFacade.java
@@ -1,10 +1,12 @@
 package kr.hhplus.be.server.application.coupon;
 
 import jakarta.transaction.Transactional;
+import java.util.concurrent.TimeUnit;
 import kr.hhplus.be.server.domain.coupon.CouponService;
 import kr.hhplus.be.server.domain.coupon.UserCoupon;
 import kr.hhplus.be.server.domain.user.User;
 import kr.hhplus.be.server.domain.user.UserService;
+import kr.hhplus.be.server.support.DistributedLock;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -15,6 +17,7 @@ public class CouponFacade {
   private final UserService userService;
   private final CouponService couponService;
 
+  @DistributedLock(key = "coupon:issue", expression = "#couponId", timeout = 30, timeUnit = TimeUnit.SECONDS)
   @Transactional
   public UserCoupon issue(Long userId, Long couponId) {
     User user = userService.findUserById(userId);
