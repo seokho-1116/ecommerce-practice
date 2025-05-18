052c1b66d758a36ee9e1f968ad682e0256c9560d
seokho <seokho7799@gmail.com>
2025-05-18 19:48:57 +0900

    feat: payment 이벤트 추가

--- src/main/java/kr/hhplus/be/server/infrastructure/payment/PaymentDataClientImpl.java
+++ src/main/java/kr/hhplus/be/server/infrastructure/payment/PaymentDataClientImpl.java
@@ -1,14 +1,14 @@
 package kr.hhplus.be.server.infrastructure.payment;
 
 import kr.hhplus.be.server.domain.payment.PaymentDataClient;
-import kr.hhplus.be.server.domain.payment.PaymentSuccessEvent;
+import kr.hhplus.be.server.domain.payment.PaymentDto.PaymentSuccessPayload;
 import org.springframework.stereotype.Component;
 
 @Component
 public class PaymentDataClientImpl implements PaymentDataClient {
 
   @Override
-  public void publish(PaymentSuccessEvent event) {
+  public void publish(PaymentSuccessPayload event) {
     // async
   }
 }
