68be39635bbc311115f4c6afdca793359a2f61a6
seokho <seokho7799@gmail.com>
2025-05-18 16:34:00 +0900

    chore: 빌드 작업, gradle 변경

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("org.springframework.boot:spring-boot-starter-cache")
+
+    // Caffeine
+    implementation("com.github.ben-manes.caffeine:caffeine")
 
     // Redis
     implementation("org.springframework.boot:spring-boot-starter-data-redis")
